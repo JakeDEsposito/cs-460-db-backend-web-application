181bfda96c9551de9bc43a735134805628f7c5fd
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-03-31 08:33:11 -0400

    Added Student and Instructor base pages

--- webAppProject_EE468/settings.py
+++ webAppProject_EE468/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'webAppProject_EE468.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'university',
+        'USER':'root',
+        'PASSWORD':'mypassword',
+        'HOST':'localhost'
     }
 }
 
--- webAppProject_EE468/views.py
+++ webAppProject_EE468/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("");
+    return HttpResponse("Login page");
