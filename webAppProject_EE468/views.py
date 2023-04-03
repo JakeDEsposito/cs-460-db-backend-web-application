0a5f3bded8626adf4fbfa6de50fadc468229dd96
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-04-03 08:25:36 -0400

    Initialized Templates

--- webAppProject_EE468/views.py
+++ webAppProject_EE468/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Login page");
+    template = loader.get_template('main/loginForm.html');
+    context = {};
+    
+    return HttpResponse(template.render(context, request));
