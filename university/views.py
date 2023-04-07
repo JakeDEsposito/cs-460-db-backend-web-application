0c800f696a3717f1b6113343fcb797c48b249120
JakeDEsposito <97902257+JakeDEsposito@users.noreply.github.com>
2023-04-07 10:18:13 -0400

    Something with models

--- university/views.py
+++ university/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def student(request):
+    return HttpResponse("student page")
+
+def instructor(request):
+    return HttpResponse("instructor page")
