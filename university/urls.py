37ac44596135cf404a5e26dc182fb9d8fbd8f057
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-04-07 11:04:14 -0400

    Readded Templates and URLs

--- university/urls.py
+++ university/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('student/', views.student)
+    path('', views.index, name='index'),
+    path('student/', views.student, name='student'),
+    path('instructor/', views.instructor, name='instructor'),
+    path('admin/', views.admin, name='admin')
 ]
