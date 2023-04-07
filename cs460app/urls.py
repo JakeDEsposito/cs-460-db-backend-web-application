958a37201290ed8aca4398ab2b2354358a8b276a
JakeDEsposito <97902257+JakeDEsposito@users.noreply.github.com>
2023-04-07 11:15:31 -0400

    Fixed error

--- cs460app/urls.py
+++ cs460app/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include('university.urls')),
     path('admin/', admin.site.urls),
 ]
