91006a03f3844a07376973f5df54b7fe36e36a1a
layan00 <layan00>
2023-04-11 11:26:48 -0400

    urls admin

--- admin/urls.py
+++ admin/urls.py
@@ -2,5 +2,10 @@ from django.urls import path, include;
 from . import views;
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.admin, name='admin'),
+    path('F1', views.F1, name='F1'),
+    path('F2', views.F2, name='F2'),
+    path('F3', views.F3, name='F3'),
+
+
 ]
