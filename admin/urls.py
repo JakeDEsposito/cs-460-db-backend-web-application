dade33c0a1730ca999e59b314bf76c6429e6174e
layan00 <layan00>
2023-04-12 10:11:51 -0400

    admin

--- admin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include;
-from . import views;
-
-urlpatterns = [
-    path('', views.admin, name='admin'),
-    path('F1', views.F1, name='F1'),
-    path('F2', views.F2, name='F2'),
-    path('F3', views.F3, name='F3'),
-]
