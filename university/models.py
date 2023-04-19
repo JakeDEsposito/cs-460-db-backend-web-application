d06bce1f27c67083a58d5ae80bd12578500c2a50
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-04-19 01:06:19 -0400

    Updated Database - Funding and Papers

--- university/models.py
+++ university/models.py
@@ -37,17 +37,58 @@ class Department(models.Model):
         db_table = 'department'
 
         
+class Funding(models.Model):
+    funding_id = models.CharField(primary_key=True, max_length=8)
+    agency = models.CharField(max_length=16, blank=True, null=True)
+    amount = models.IntegerField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'funding'
+
+
+class FundingAwardee(models.Model):
+    funding = models.ForeignKey(Funding, models.DO_NOTHING, primary_key=True)
+    instructor = models.ForeignKey('Instructor', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'funding_awardee'
+        unique_together = (('funding', 'instructor'),)
+
+
 class Instructor(models.Model):
     id = models.CharField(db_column='ID', primary_key=True, max_length=8)  # Field name made lowercase.
     name = models.CharField(max_length=20, blank=True, null=True)
     dept_name = models.ForeignKey(Department, models.DO_NOTHING, db_column='dept_name', blank=True, null=True)
     salary = models.IntegerField(blank=True, null=True)
-    funding = models.IntegerField(blank=True, null=True)
-    papers_published = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'instructor'
+        
+
+class Paper(models.Model):
+    paper_id = models.CharField(primary_key=True, max_length=8)
+    title = models.CharField(max_length=64, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    publication_venue = models.CharField(max_length=32, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'paper'
+
+
+class PaperAuthor(models.Model):
+    paper = models.ForeignKey(Paper, models.DO_NOTHING, primary_key=True)
+    instructor = models.ForeignKey(Instructor, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'paper_author'
+        unique_together = (('paper', 'instructor'),)
 
 
 class Prereq(models.Model):
